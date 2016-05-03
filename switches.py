020cb49769032cc65494455716feac80cd984f7a
Somchai Somphadung <somchaisomph@gmail.com>
2016-05-03 12:47:52 +0700

    Update switches.py

--- switches.py
+++ switches.py
@@ -13,11 +13,30 @@ GPIO.setup(SWITCH_C,GPIO.IN)
 GPIO.setup(SWITCH_D,GPIO.IN)
 
 input_a = GPIO.input(SWITCH_A)
-input_a = GPIO.input(SWITCH_A)
-while True:
-	if GPIO.input(26) :
-		print "Released"
-	else:
-		print "Pressed"
+input_b = GPIO.input(SWITCH_B)
+input_c = GPIO.input(SWITCH_C)
+input_d = GPIO.input(SWITCH_D)
 
-GPIO.cleanup()
+while True:
+	if GPIO.input(SWITCH_A) :
+		print "A Released"
+	else:
+		print "A Pressed"
+
+	if GPIO.input(SWITCH_B) :
+		print "B Released"
+	else:
+		print "B Pressed"
+
+	if GPIO.input(SWITCH_C) :
+		print "C Released"
+	else:
+		print "C Pressed"
+		
+	if GPIO.input(SWITCH_D) :
+		print "D Released"
+	else:
+		print "D Pressed"
+
+
+GPIO.cleanup([SWITCH_A,SWITCH_B,SWITCH_C,SWITCH_D])
